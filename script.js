76a27040c8758bc2fc3f3a060bf7bc8539d8d21a
jaytrive <113422452+jaytrive@users.noreply.github.com>
2023-04-05 01:06:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -496,14 +496,3 @@ function won() {
         boardCell.removeEventListener("contextmenu", addingFlag)
     })
 }
-  
-document.getElementById('reset_button').addEventListener('click', reset);
-
-document.getElementById('change_button').addEventListener('click', () => {
-    reset();
-    const blockId = document.getElementById("block_id").value;
-    const color = document.getElementById("colour_id").value;
-    // alert(colorId)
-    const block = document.getElementById(`${blockId}`);
-    block.style.backgroundColor = color;
-});
